c5d8a3736572db3640b38fe4311f651bcb95fad0
toyshe <toyshehossain@outlook.com>
2024-12-04 16:42:25 +0000

    adding post method for admin

--- Backend/controllers/admin.controller.js
+++ Backend/controllers/admin.controller.js
@@ -1,7 +1,16 @@
-const { findAdmins } = require("../models/admins.models")
+const { findAdmins, insertAdmins } = require("../models/admins.models");
 
 exports.getAdmins = (req, res, next) => {
-    findAdmins().then((admins) => {
-        res.status(200).send({admins})
+  findAdmins().then((admins) => {
+    res.status(200).send({ admins });
+  });
+};
+
+exports.postAdmins = (req, res, next) => {
+  const body = req.body;
+  insertAdmins(body)
+    .then((admin) => {
+      res.status(201).send({ admin });
     })
-}
+    .catch(next);
+};
--- Backend/models/admins.models.js
+++ Backend/models/admins.models.js
@@ -5,3 +5,10 @@ exports.findAdmins = () => {
         return rows
     })
 }
+
+exports.insertAdmins = ({username, password, firstname, lastname, address, phone, position, email}) => {
+    return db.query(`INSERT INTO admins (username, password, firstname, lastname, email, phone, address, position) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`, [username, password, firstname, lastname, email, phone, address, position])
+    .then(({rows}) => {
+        return rows[0]
+    })
+}
